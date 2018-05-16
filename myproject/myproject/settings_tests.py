e3f820f481bf384d8fe0501a6703dc1a9f12b539
phil <phil@potatolondon.com>
2018-05-16 20:41:04 +0100

    Enable test coverage for myapp
    Please re-run `pip install -r requirements.txt`

--- myproject/myproject/settings_tests.py
+++ myproject/myproject/settings_tests.py
@@ -14,4 +14,8 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 # see `./manage.py test --help` for all options 
 NOSE_ARGS = [
     '--failed',  # Run the tests that failed in the last test run
+    
+    # coverage
+    '--with-coverage',
+    '--cover-package=myapp',
 ]
