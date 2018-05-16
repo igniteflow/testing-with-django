38c3455b28e3c52f84534d0efaf86cf51904312d
phil <phil@potatolondon.com>
2018-05-16 20:33:27 +0100

    Use django-nose
    Run `pip install -r requirements.txt` to install

--- myproject/myproject/settings_tests.py
+++ myproject/myproject/settings_tests.py
@@ -3,3 +3,15 @@ from .settings import *
 MIGRATION_MODULES = {
     'myapp': None,
 }
+
+INSTALLED_APPS += [
+    'django_nose',
+]
+
+# django-nose
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+# see `./manage.py test --help` for all options 
+NOSE_ARGS = [
+    '--failed',  # Run the tests that failed in the last test run
+]
